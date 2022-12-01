bd32e80dd622cebce5d8ea56eefd7ec625c85f47
Robert Macaulay <robert.macaulay@gmail.com>
2022-12-01 17:03:40 -0600

    Day1

--- src/Day01.kt
+++ src/Day01.kt
@@ -1,15 +1,24 @@
 fun main() {
+    fun sumElves(input: List<String>): List<Int> {
+        return input.asSequence()
+            .split { it.isBlank() }
+            .map { it.sumOf { a -> a.toInt()} }
+            .toList()
+    }
+
     fun part1(input: List<String>): Int {
-        return input.size
+        val elves = sumElves(input)
+        return elves.max()
     }
 
     fun part2(input: List<String>): Int {
-        return input.size
+        val elves = sumElves(input)
+        return elves.sortedDescending().take(3).sum()
     }
 
-    // test if implementation meets criteria from the description, like:
     val testInput = readInput("Day01_test")
-    check(part1(testInput) == 1)
+    check(part1(testInput) == 24000)
+    check(part2(testInput) == 45000)
 
     val input = readInput("Day01")
     println(part1(input))
--- src/Utils.kt
+++ src/Utils.kt
@@ -2,15 +2,30 @@ import java.io.File
 import java.math.BigInteger
 import java.security.MessageDigest
 
-/**
- * Reads lines from the given input txt file.
- */
 fun readInput(name: String) = File("src", "$name.txt")
     .readLines()
 
-/**
- * Converts string to md5 hash.
- */
 fun String.md5() = BigInteger(1, MessageDigest.getInstance("MD5").digest(toByteArray()))
     .toString(16)
     .padStart(32, '0')
+
+
+fun <T> Sequence<T>.split(predicate: (T) -> Boolean): Sequence<List<T>> {
+    val iterator = this.iterator()
+    val buffer = mutableListOf<T>()
+
+    return sequence {
+        while (iterator.hasNext()) {
+            val element = iterator.next()
+            if (predicate.invoke(element)) {
+                yield(buffer.toList())
+                buffer.clear()
+            } else {
+                buffer.add(element)
+            }
+        }
+        if (buffer.isNotEmpty()) {
+            yield(buffer.toList())
+        }
+    }
+}
